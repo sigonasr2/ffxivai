b47804c51a8a4b7eb24b57e632489780aa301bf3
Nic0Nic0Nii <sigonasr2@yahoo.com>
2021-10-04 06:47:02 +0000

    Frame rate checker and limit warning.
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

--- src/sig/FFXIV.java
+++ src/sig/FFXIV.java
@@ -7,7 +7,7 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class FFXIV{
-    public long FRAMETIME=0;
+    public static double FRAMETIME=0;
     public static Robot r;
     public static void main(String[] args) {
         try {
@@ -23,7 +23,11 @@ public class FFXIV{
                 e.printStackTrace();
             }
         }
-        System.out.println("60 captures took "+((double)((int)(System.nanoTime()-startTime)/10000000)/100)+"s");
+        FRAMETIME = ((double)((long)(System.nanoTime()-startTime)/10000000)/100);
+        System.out.println("60 captures took "+FRAMETIME+"s. FPS:"+Math.floor(60/FRAMETIME));
+        if (60/FRAMETIME<10) {
+            System.out.println(" WARNING!! For best performance, it's highly recommended to use this on a computer that can compute at least 10 frames per second!");
+        }
         System.out.println("Hello World!");
     }
     private static BufferedImage CaptureScreen() throws IOException {
