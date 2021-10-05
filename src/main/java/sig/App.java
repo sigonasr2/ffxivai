332973fedf9ff702295bae3a59e0a722a2f377db
Nic0Nic0Nii <sigonasr2@yahoo.com>
2021-10-05 00:41:33 +0000

    setup maven project
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

--- src/main/java/sig/App.java
+++ src/main/java/sig/App.java
@@ -1,4 +1,4 @@
-package sig;
+package main.java.sig;
 
 import java.awt.AWTException;
 import java.awt.Robot;
@@ -6,15 +6,22 @@ import java.awt.GraphicsEnvironment;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
-public class FFXIV{
+import javax.swing.JFrame;
+
+public class App{
     public static double FRAMETIME=0;
     public static Robot r;
+    public static JFrame f;
     public static void main(String[] args) {
         try {
             r = new Robot();
         } catch (AWTException e) {
             e.printStackTrace();
         }
+        RunInitialAnalysis();
+        f = new JFrame("FFXIV AI");
+    }
+    private static void RunInitialAnalysis() {
         long startTime = System.nanoTime();
         for (int i=0;i<60;i++) {
             try {
@@ -28,7 +35,6 @@ public class FFXIV{
         if (60/FRAMETIME<10) {
             System.out.println(" WARNING!! For best performance, it's highly recommended to use this on a computer that can compute at least 10 frames per second!");
         }
-        System.out.println("Hello World!");
     }
     private static BufferedImage CaptureScreen() throws IOException {
 		BufferedImage screenshot = r.createScreenCapture(GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds());
